c51174df014246613bde9f371740f99d529ed277
Joseph Koshakow <koshy44@gmail.com>
2024-09-01 09:56:03 -0700

    Complete week 1 day 2 (#2)

--- mini-lsm-starter/src/iterators/merge_iterator.rs
+++ mini-lsm-starter/src/iterators/merge_iterator.rs
@@ -1,6 +1,3 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 use std::cmp::{self};
 use std::collections::BinaryHeap;
 
@@ -45,7 +42,58 @@ pub struct MergeIterator<I: StorageIterator> {
 
 impl<I: StorageIterator> MergeIterator<I> {
     pub fn create(iters: Vec<Box<I>>) -> Self {
-        unimplemented!()
+        let mut iters: BinaryHeap<HeapWrapper<I>> = iters
+            .into_iter()
+            .filter(|iter| iter.is_valid())
+            .enumerate()
+            .map(|(idx, iter)| HeapWrapper(idx, iter))
+            .collect();
+        let current = iters.pop();
+        Self { iters, current }
+    }
+}
+
+impl<I: 'static + for<'a> StorageIterator<KeyType<'a> = KeySlice<'a>>> MergeIterator<I> {
+    /// Advance all iters in `self.iters` s.t. they are all less than `self.current`.
+    fn advance_iters(&mut self) -> Result<()> {
+        if !self.is_valid() {
+            return Ok(());
+        }
+
+        while let Some(iter) = self.iters.peek() {
+            if iter.1.key() > self.key() {
+                return Ok(());
+            }
+
+            let mut iter = self.iters.pop().expect("peeked above");
+            match self.advance_iter(&mut iter) {
+                Ok(()) => {
+                    if iter.1.is_valid() {
+                        self.iters.push(iter)
+                    }
+                }
+                e @ Err(_) => return e,
+            }
+        }
+
+        if let Some(current) = &self.current {
+            assert!(self
+                .iters
+                .iter()
+                .all(|iter| iter.1.is_valid() && iter.1.key() > current.1.key()));
+        }
+
+        Ok(())
+    }
+
+    /// Advance `iter` s.t. it is less than `self.current`.
+    fn advance_iter(&self, iter: &mut HeapWrapper<I>) -> Result<()> {
+        assert!(iter.1.is_valid());
+        while iter.1.is_valid() && iter.1.key() <= self.key() {
+            iter.1.next()?;
+        }
+        assert!(!iter.1.is_valid() || iter.1.key() > self.key());
+        Ok(())
     }
 }
 
@@ -55,18 +103,31 @@ impl<I: 'static + for<'a> StorageIterator<KeyType<'a> = KeySlice<'a>>> StorageIt
     type KeyType<'a> = KeySlice<'a>;
 
     fn key(&self) -> KeySlice {
-        unimplemented!()
+        self.current.as_ref().expect("is valid").1.key()
     }
 
     fn value(&self) -> &[u8] {
-        unimplemented!()
+        self.current.as_ref().expect("is valid").1.value()
     }
 
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        self.current.is_some()
     }
 
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        if !self.is_valid() {
+            return Ok(());
+        }
+        self.advance_iters()?;
+
+        let mut current = self.current.take().expect("is valid");
+        current.1.next()?;
+        if current.1.is_valid() {
+            self.iters.push(current);
+        }
+        self.current = self.iters.pop();
+        self.advance_iters()?;
+
+        Ok(())
     }
 }
