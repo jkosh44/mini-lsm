80be26ca17429b1ab4ef254ec09e71e79398ce20
Joseph Koshakow <koshy44@gmail.com>
2024-09-02 12:29:42 -0700

    Complete week 1 day 3 (#4)

--- mini-lsm-starter/src/block/builder.rs
+++ mini-lsm-starter/src/block/builder.rs
@@ -1,6 +1,3 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 use crate::key::{KeySlice, KeyVec};
 
 use super::Block;
@@ -20,22 +17,58 @@ pub struct BlockBuilder {
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        Self {
+            offsets: Vec::new(),
+            data: Vec::new(),
+            block_size,
+            first_key: KeyVec::new(),
+        }
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: KeySlice, value: &[u8]) -> bool {
-        unimplemented!()
+        let mut first_key_short_circuit = false;
+        if self.is_empty() {
+            self.first_key = key.to_key_vec();
+            first_key_short_circuit = true;
+        }
+
+        self.offsets.push(self.data.len() as u16);
+        let key_len = key.len() as u16;
+        let value_len = value.len() as u16;
+        let bytes = std::iter::empty()
+            .chain(key_len.to_le_bytes())
+            .chain(key.raw_ref().iter().cloned())
+            .chain(value_len.to_le_bytes())
+            .chain(value.iter().cloned());
+        self.data.extend(bytes);
+
+        let size = self.size();
+        first_key_short_circuit || size < self.block_size
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        assert_eq!(self.offsets.is_empty(), self.data.is_empty());
+        assert_eq!(self.first_key.is_empty(), self.data.is_empty());
+        self.data.is_empty()
     }
 
     /// Finalize the block.
     pub fn build(self) -> Block {
-        unimplemented!()
+        Block {
+            data: self.data,
+            offsets: self.offsets,
+        }
+    }
+
+    fn size(&self) -> usize {
+        // data
+        self.data.len() +
+            // offsets
+            (self.offsets.len() * size_of::<u16>()) +
+            // num_elements
+            size_of::<u16>()
     }
 }
