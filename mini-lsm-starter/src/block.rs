94c8d4c4791bff2ada3dd0b3f962c4dfd3c7d37c
Joseph Koshakow <koshy44@gmail.com>
2024-09-02 15:23:52 -0400

    Complete week 1 day 3

--- mini-lsm-starter/src/block.rs
+++ mini-lsm-starter/src/block.rs
@@ -1,12 +1,12 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 mod builder;
 mod iterator;
 
 pub use builder::BlockBuilder;
 use bytes::Bytes;
 pub use iterator::BlockIterator;
+
+const NUM_ELEMENTS_SIZE: usize = size_of::<u16>();
+const OFFSET_SIZE: usize = size_of::<u16>();
 
 /// A block is the smallest unit of read and caching in LSM tree. It is a collection of sorted key-value pairs.
 pub struct Block {
@@ -18,11 +18,38 @@ impl Block {
     /// Encode the internal data to the data layout illustrated in the tutorial
     /// Note: You may want to recheck if any of the expected field is missing from your output
     pub fn encode(&self) -> Bytes {
-        unimplemented!()
+        let num_elements = self.offsets.len() as u16;
+        self.data
+            .iter()
+            .cloned()
+            .chain(
+                self.offsets
+                    .iter()
+                    .flat_map(|offset| offset.to_le_bytes().into_iter()),
+            )
+            .chain(num_elements.to_le_bytes())
+            .collect()
     }
 
     /// Decode from the data layout, transform the input `data` to a single `Block`
     pub fn decode(data: &[u8]) -> Self {
-        unimplemented!()
+        let n = data.len();
+        let offset = n - NUM_ELEMENTS_SIZE;
+        let (data, num_elements) = (&data[..offset], &data[offset..]);
+        let n = data.len();
+        let num_elements = u16::from_le_bytes(num_elements.try_into().expect("correct size"));
+        let offset = n - (num_elements as usize * OFFSET_SIZE);
+        let (data, offsets) = (&data[..offset], &data[offset..]);
+        let offsets: Vec<_> = offsets
+            .chunks(OFFSET_SIZE)
+            .map(|offset| u16::from_le_bytes(offset.try_into().expect("correct size")))
+            .collect();
+        let data = data.to_vec();
+
+        Self { data, offsets }
+    }
+
+    pub(crate) fn num_elements(&self) -> usize {
+        self.offsets.len()
     }
 }
--- mini-lsm-starter/src/block/iterator.rs
+++ mini-lsm-starter/src/block/iterator.rs
@@ -1,11 +1,11 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 use std::sync::Arc;
 
 use crate::key::{KeySlice, KeyVec};
 
 use super::Block;
+
+const KEY_LEN_SIZE: usize = size_of::<u16>();
+const VALUE_LEN_SIZE: usize = size_of::<u16>();
 
 /// Iterates on a block.
 pub struct BlockIterator {
@@ -34,44 +34,77 @@ impl BlockIterator {
 
     /// Creates a block iterator and seek to the first entry.
     pub fn create_and_seek_to_first(block: Arc<Block>) -> Self {
-        unimplemented!()
+        let mut iter = Self::new(block);
+        iter.seek_to_first();
+        iter.first_key = iter.key.clone();
+        iter
     }
 
     /// Creates a block iterator and seek to the first key that >= `key`.
     pub fn create_and_seek_to_key(block: Arc<Block>, key: KeySlice) -> Self {
-        unimplemented!()
+        let mut iter = Self::create_and_seek_to_first(block);
+        iter.seek_to_key(key);
+        iter
     }
 
     /// Returns the key of the current entry.
     pub fn key(&self) -> KeySlice {
-        unimplemented!()
+        self.key.as_key_slice()
     }
 
     /// Returns the value of the current entry.
     pub fn value(&self) -> &[u8] {
-        unimplemented!()
+        &self.block.data[self.value_range.0..self.value_range.1]
     }
 
     /// Returns true if the iterator is valid.
     /// Note: You may want to make use of `key`
     pub fn is_valid(&self) -> bool {
-        unimplemented!()
+        !self.key.is_empty()
     }
 
     /// Seeks to the first key in the block.
     pub fn seek_to_first(&mut self) {
-        unimplemented!()
+        self.idx = 0;
+        self.next_inner();
     }
 
     /// Move to the next key in the block.
     pub fn next(&mut self) {
-        unimplemented!()
+        self.idx += 1;
+        self.next_inner();
+    }
+
+    fn next_inner(&mut self) {
+        if self.idx < self.block.num_elements() {
+            let offset = self.block.offsets[self.idx] as usize;
+            let data = &self.block.data[offset..];
+            let (key_len, data) = (&data[..KEY_LEN_SIZE], &data[KEY_LEN_SIZE..]);
+            let key_len = u16::from_le_bytes(key_len.try_into().expect("correct size")) as usize;
+            let (key, data) = (&data[..key_len], &data[key_len..]);
+
+            self.key = KeyVec::from_vec(key.to_vec());
+
+            let value_len = &data[..VALUE_LEN_SIZE];
+            let value_len =
+                u16::from_le_bytes(value_len.try_into().expect("correct size")) as usize;
+
+            let value_start = offset + KEY_LEN_SIZE + key_len + VALUE_LEN_SIZE;
+            let value_end = value_start + value_len;
+
+            self.value_range = (value_start, value_end);
+        } else {
+            self.key = KeyVec::new();
+        }
     }
 
     /// Seek to the first key that >= `key`.
     /// Note: You should assume the key-value pairs in the block are sorted when being added by
     /// callers.
     pub fn seek_to_key(&mut self, key: KeySlice) {
-        unimplemented!()
+        self.seek_to_first();
+        while self.is_valid() && self.key() < key {
+            self.next();
+        }
     }
 }
